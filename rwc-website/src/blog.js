cd72de7c537e278ef8b72ebd8fa039585138f694
AnikaMahesh <52103193+AnikaMahesh@users.noreply.github.com>
2021-01-30 18:32:50 -0800

    Update blog.js

--- rwc-website/src/blog.js
+++ rwc-website/src/blog.js
@@ -1,10 +1,22 @@
 function Blog() {
     return (
-        <div className="header">
+        <div className="header" background> 
+            <h1>What's New</h1>
             <hr />
-            <h1>blog</h1>
-            <p><i>Our main goal is to assist in reducing the quanity of malicious contaminants present in much of our current-day water utilizing our 'SortCan'. This new-age garbage can has the capability to sort the user's garbage into certain sub-cans dependent on the type of trash being disposed of with input from the user.</i></p>
-            <br />
+            <div style="background-image:url('https://domain5127290.files.wordpress.com/2021/01/bin-1.png?w=77'); text-align:center;  background-size:200px,200px; color:blue">
+            <div style="background-image:url('https://www.weitzlux.com/wp-content/uploads/water-contamination__hero.jpg'); text-align:center; background-size:400px,400px; color:white">
+            <h2>Water Contamination Is Not Stopping, Act Now!</h2>
+            <h3>1/31/2021</h3>
+            <p style="text-size: '20px';">Everyday millions of gallons of pollutants polute the water, stemming from agriculture, trash and many other soures. This is bringing strife to many urban towns and cities. However, you can halp stop this, Buy RWC's newest product, the SortCan.</p>
+            </div>
+            <h2>The New Sortcan just got Modeled</h2>
+            <h3>1/30/2021</h3>
+            <p style="text-size: '20px'">The SortCan is a revolutionary technology that sorts consumerables based on barcode. Contains a lcd screen for customization. With new updates new innovation, the RWC brings you the device that will improve water quality for many years to come.</p>
+            </div>
+            <div style="background-image:url('http://wallpaperheart.com/wp-content/uploads/2018/03/high-definition-water-wallpapers-ocean-wallpapers.jpg'); height; background-size:400px,400px">
+            <h2>New Release!</h2>
+            <h3>1/30/2021</h3>
+            <p style="text-size: '20px'">As said in it's mission statement, The SortCan is a revolutionary device designed to revolvutionize recycling and therfore, reducing the flow of pollutants into the waterways.</p></div>
             <hr />
         </div>
     );
