6631bda97c5d18975bd883d619adcaff82ade297
AnikaMahesh <52103193+AnikaMahesh@users.noreply.github.com>
2021-01-30 16:37:52 -0800

    Update contact.js

--- rwc-website/src/contact.js
+++ rwc-website/src/contact.js
@@ -4,6 +4,12 @@ function Contact() {
             <hr />
             <h1>contact</h1>
             <p><i>Our main goal is to assist in reducing the quanity of malicious contaminants present in much of our current-day water utilizing our 'SortCan'. This new-age garbage can has the capability to sort the user's garbage into certain sub-cans dependent on the type of trash being disposed of with input from the user.</i></p>
+                <div className="header" style="text-align:center;">
+                <br></br><br></br><br></br><br></br>
+                <h3 style='text-align:center'>Keep In Touch!</h3>
+                <a href="https://www.facebook.com/joana.doe.334" style='text-align:center'><img src='https://pngmind.com/wp-content/uploads/2019/08/Facebook-Icon-Png-Transparent-Background.jpeg' height='50'></img></a><a href="https://www.instagram.com/"><img src='https://www.philosopher.eu/wp-content/uploads/2013/10/Instagram-logo.png'height='50' ></img></a><a href="https://www.youtube.com/channel/UCSSbZRKHS72VuwhjX0-APKg"><img src='https://cdn.wccftech.com/wp-content/uploads/2015/03/371_youtube1.jpg' height='60'></img></a>
+                <br></br><br></br><br></br><br></br><br></br>
+            </div>
             <br />
             <hr />
         </div>
